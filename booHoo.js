d04b4983e8f0b150ddd5909cf596705511adfa6a
snelakanti <vnelakanti@gmail.com>
2019-03-31 11:19:51 -0600

    boo hoo

---
+++ booHoo.js
@@ -0,0 +1,13 @@
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if (bool === true || bool === false) {
+    return true;
+  }
+  return false;
+}
+
+booWho(null);
+
+//Check if a value is classified as a boolean primitive. Return true or false.
+
+//Boolean primitives are true and false.
