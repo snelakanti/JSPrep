0387de1595ecca171ca8edff17a8096992c2b012
snelakanti <vnelakanti@gmail.com>
2019-04-08 12:56:24 -0600

    remove false values

---
+++ falsyBouncer.js
@@ -0,0 +1,17 @@
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  let arr1 = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Boolean(arr[i]) !== false) {
+      arr1.push(arr[i]);
+    }
+  }
+  console.log(arr1);
+  return arr1;
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+// let one = false;
+// let two = Boolean(one);
+// console.log(two); //false
