a491feee25e96731194dc13cf3b24a3ae5d1eeb5
snelakanti <vnelakanti@gmail.com>
2019-04-08 13:29:14 -0600

    find index

---
+++ whereIBelong.js
@@ -0,0 +1,13 @@
+function getIndexToIns(arr, num) {
+  // Find my place in this sorted array.
+  arr.push(num);
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+  return arr.indexOf(num);
+}
+
+getIndexToIns([40, 60], 50);
+
+//sort() - descending order
+//sort(function(a,b){b-a}) - ascending
